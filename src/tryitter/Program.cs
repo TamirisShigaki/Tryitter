7a9dc6f06977c7b846474103f0073d4b871016d5
TamirisShigaki <tamiris.endo@altaplus.com.br>
2023-05-27 18:47:47 -0300

    feat: :building_construction: configuração do banco de dados

---
+++ src/tryitter/Program.cs
@@ -0,0 +1,31 @@
+using tryitter.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TryitterContext>();
+builder.Services.AddScoped<ITryitterContext, TryitterContext>();
+// builder.Services.AddScoped<ITryitterRepository, TryitterRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { }
