5614fa1a11ea08d3d6bb05450917a2cb4e5bf35d
TamirisShigaki <tamiris.endo@altaplus.com.br>
2023-05-31 22:50:25 -0300

    feat: :construction: repository

--- src/tryitter/Repository/TryitterContext.cs
+++ src/tryitter/Repository/TryitterContext.cs
@@ -3,24 +3,33 @@ using tryitter.Models;
 
 namespace tryitter.Repository;
 
-public class TryitterContext : DbContext, ITryitterContext
+public class TryitterContext : DbContext
 {
-    public DbSet<Studant> Studants { get; set; }
+    public DbSet<Post>? Posts { get; set; }
 
-    public DbSet<Post> Posts { get; set; }
+    public DbSet<Student>? Students { get; set; }
 
-    public TryitterContext(DbContextOptions<TryitterContext> options) : base(options) { }
-    public TryitterContext() { }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
         {
-            optionsBuilder.UseSqlServer(@"
-                Server=localhost;
-                Database=TryitterDB;
-                User Id=sa;
-                Password=Password12!;
-            ");
+            var connectionString = Environment.GetEnvironmentVariable("DOTNET_CONNECTION_STRING");
+
+            optionsBuilder.UseSqlServer(@"Server=127.0.0.1;Database=TryitterDB;User=SA;Password=Password12;");
         }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>()
+        .HasKey(x => x.StudentId);
+
+        modelBuilder.Entity<Post>()
+        .HasKey(x => x.PostId);
+
+        modelBuilder.Entity<Post>()
+        .HasOne(c => c.Student)
+        .WithMany(x => x.Posts)
+        .HasForeignKey(d => d.StudentId);
     }
 }
